abacd352bf49714d591c6def237aaee10e3f1f10
djones <dgjones@gmail.com>
2010-10-06 15:27:22 +1300

    putting things more in line. the tab does show at this point, just need to get it working

--- lib/refinerycms-page-images.rb
+++ lib/refinerycms-page-images.rb
@@ -7,13 +7,28 @@ module Refinery
         app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public"
       end
       
-      config.after_initialize do
-        <% # need to implement this in Refinery yet %>
-        Refinery::Pages.register_tab do |tab|
-          tab.name = "images"
-          tab.template = "/admin/pages/tabs/images"
+      config.to_prepare do
+        Page.module_eval do
+
+          has_and_belongs_to_many :images
+          accepts_nested_attributes_for :images, :allow_destroy => false
+
+          def images_attributes=(data)
+            self.images.clear
+
+            self.images += (0..(data.length-1)).collect { |i|
+              (Image.find(data[i.to_s]['id'].to_i) rescue nil)
+            }.compact
+          end
         end
-        
+      end
+      
+      config.after_initialize do
+
+        Refinery::Pages::Tab.register do |tab|
+          tab.name = "images"
+          tab.partial = "/admin/pages/tabs/images"
+        end
       end
     end
   end
--- refinerycms-page-images.gemspec
+++ refinerycms-page-images.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.platform          = Gem::Platform::RUBY
   s.name              = 'refinerycms-page-images'
-  s.version           = '1.0'
+  s.version           = '0.9'
   s.description       = 'Page Images Engine for Refinery CMS'
   s.date              = '2010-10-05'
   s.summary           = 'Page Images Engine for Refinery CMS'
