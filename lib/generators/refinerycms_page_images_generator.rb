1a0e0aef2c00aa010c1892cc81c28e3f6b001b95
Philip Arndt <parndt@gmail.com>
2010-12-21 12:13:30 +1300

    Fix path for being one level deeper (inside refinery/ now)

--- lib/generators/refinerycms_page_images_generator.rb
+++ lib/generators/refinerycms_page_images_generator.rb
@@ -1,6 +1,6 @@
 class RefinerycmsPageImages < Refinery::Generators::EngineInstaller
 
-  source_root File.expand_path('../../', __FILE__)
+  source_root File.expand_path('../../../', __FILE__)
   engine_name "page_images"
 
 end
