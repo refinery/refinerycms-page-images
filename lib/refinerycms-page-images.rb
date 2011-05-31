be4740a2b0d131b96aa9ea296b7a5a8dca8f1969
Michael Gall <michael@wakeless.net>
2011-05-31 13:36:36 +1000

    test images_attributes setting and fix indenting

--- lib/refinerycms-page-images.rb
+++ lib/refinerycms-page-images.rb
@@ -58,33 +58,33 @@ module Refinery
           accepts_nested_attributes_for :images, :allow_destroy => false
           module_eval do ## need to do it this way because of the way accepts_nested_attributes_for deletes an already defined images_attributes
             def images_attributes=(data)
-            ids_to_delete = data.map{|i, d| d['image_page_id']}.compact
-            self.image_pages.each do |image_page|
-              if ids_to_delete.index(image_page.id.to_s).nil?
-                # Image has been removed, we must delete it
-                self.image_pages.delete(image_page)
-                image_page.destroy
+              ids_to_delete = data.map{|i, d| d['image_page_id']}.compact
+              self.image_pages.each do |image_page|
+                if ids_to_delete.index(image_page.id.to_s).nil?
+                  # Image has been removed, we must delete it
+                  self.image_pages.delete(image_page)
+                  image_page.destroy
+                end
+              end
+              
+              (0..(data.length-1)).each do |i|
+                unless (image_data = data[i.to_s]).nil? or image_data['id'].blank?
+                  image_page = if image_data['image_page_id'].present?
+                    self.image_pages.find(image_data['image_page_id'])
+                  else
+                    self.image_pages.new(:image_id => image_data['id'].to_i)
+                  end
+                  image_page.position = i
+                  # Add caption if supported
+                  if RefinerySetting.find_or_set(:page_images_captions, false)
+                    image_page.caption = image_data['caption']
+                  end
+                  
+                  self.image_pages << image_page if image_data['image_page_id'].blank?
+                  image_page.save
+                end
               end
             end
-            
-            (0..(data.length-1)).each do |i|
-              unless (image_data = data[i.to_s]).nil? or image_data['id'].blank?
-                image_page = if image_data['image_page_id'].present?
-                  self.image_pages.find(image_data['image_page_id'])
-                else
-                  self.image_pages.new(:image_id => image_data['id'].to_i)
-                end
-                image_page.position = i
-                # Add caption if supported
-                if RefinerySetting.find_or_set(:page_images_captions, false)
-                  image_page.caption = image_data['caption']
-                end
-                
-                self.image_pages << image_page if image_data['image_page_id'].blank?
-                image_page.save
-              end
-            end
-          end
           end
     
           include Refinery::PageImages::Extension::InstanceMethods
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -9,4 +9,14 @@ describe Page do
     p.images.count.should eql(1) 
   end
   
+  it "should accept images_attributes=" do
+    page = Factory(:page)
+    image = Factory(:image)
+    
+    page.images.count.should eql(0)
+    page.update_attributes({:images_attributes => {"0" => {"id" => image.id}}})
+    
+    page.images.count.should eql(1)
+  end
+  
 end
