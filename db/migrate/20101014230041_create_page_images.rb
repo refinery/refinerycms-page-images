72d6a48a7af5bc9422fe3f188731a0da8066c972
Uģis Ozols <ugis.ozolss@gmail.com>
2011-08-13 14:19:53 +0300

    Update migrations.

--- db/migrate/20101014230041_create_page_images.rb
+++ db/migrate/20101014230041_create_page_images.rb
@@ -1,18 +1,12 @@
 class CreatePageImages < ActiveRecord::Migration
-
-  def self.up
-    create_table :image_pages, :id => false do |t|
+  def change
+    create_table Refinery::ImagePage.table_name, :id => false do |t|
       t.integer :image_id
       t.integer :page_id
       t.integer :position
     end
 
-    add_index :image_pages, :image_id
-    add_index :image_pages, :page_id
+    add_index Refinery::ImagePage.table_name, :image_id
+    add_index Refinery::ImagePage.table_name, :page_id
   end
-
-  def self.down
-    drop_table :image_pages
-  end
-
 end
