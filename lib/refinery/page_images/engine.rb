579c98206b2423fe475bed3a9392b73133409862
Rich Larcombe <richlarcombe@gmail.com>
2012-08-24 23:13:09 +1000

    allow 'Image' tab to be renamed in the translation - i.e to slideshow

--- lib/refinery/page_images/engine.rb
+++ lib/refinery/page_images/engine.rb
@@ -8,7 +8,7 @@ module Refinery
       engine_name :refinery_page_images
 
       def self.register(tab)
-        tab.name = "images"
+        tab.name = ::I18n.t(:'refinery.plugins.refinery_page_images.tab_name')
         tab.partial = "/refinery/admin/pages/tabs/images"
       end
 
