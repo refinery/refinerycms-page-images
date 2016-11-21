470655a6b239b103ae9c466cac64853de16f067b
Brice Sanchez <contact@brice-sanchez.com>
2016-11-21 12:26:39 -0500

    Add test for touching

--- spec/factories/page-images.rb
+++ spec/factories/page-images.rb
@@ -6,4 +6,9 @@ FactoryGirl.define do
   factory :blog_post_with_image, :parent => :blog_post do
     after(:create) { |b| b.image_pages.create(image: FactoryGirl.create(:image)) }
   end if defined? Refinery::Blog::Post
+
+  factory :image_page, class: Refinery::ImagePage do
+    image
+    page
+  end
 end
