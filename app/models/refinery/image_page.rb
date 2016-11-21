969042b5af0612126667ad477fd160c6acbedea7
Brice Sanchez <contact@brice-sanchez.com>
2016-11-21 12:27:27 -0500

    Add touch: true to belongs_to :page in ImagePage model

--- app/models/refinery/image_page.rb
+++ app/models/refinery/image_page.rb
@@ -2,7 +2,7 @@ module Refinery
   class ImagePage < Refinery::Core::BaseModel
 
     belongs_to :image
-    belongs_to :page, :polymorphic => true
+    belongs_to :page, polymorphic: true, touch: true
 
     translates :caption
   end
