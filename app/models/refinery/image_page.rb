76e49d1aaa0bae57c6caee72d8bd1cc5c9a76c79
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-05-03 13:17:36 -0400

    Add optional: true to belongs_to relation with :page

--- app/models/refinery/image_page.rb
+++ app/models/refinery/image_page.rb
@@ -2,7 +2,7 @@ module Refinery
   class ImagePage < Refinery::Core::BaseModel
 
     belongs_to :image
-    belongs_to :page, polymorphic: true, touch: true
+    belongs_to :page, polymorphic: true, touch: true, optional: true
 
     extend Mobility
     translates :caption
