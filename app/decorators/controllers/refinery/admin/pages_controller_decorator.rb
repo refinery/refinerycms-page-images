6032bd2b143a2c97d8b916e9f1b6c87dfddc6fd1
Brice Sanchez <bricesanchez@users.noreply.github.com>
2018-08-29 08:13:57 -0400

    Now use Module#prepend for decorators

--- app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/pages_controller_decorator.rb
@@ -1,10 +1,8 @@
-if defined?(Refinery::Page)
-  Refinery::Admin::PagesController.prepend(
-    Module.new do
-      def permitted_page_params
-        params[:page][:images_attributes]={} if params[:page][:images_attributes].nil?
-        super <<  [images_attributes: [:id, :caption, :image_page_id]]
-      end
-    end
-  )
+module RefineryPageImagesAddRefineryAdminPagesControllerParams
+  def permitted_page_params
+    params[:page][:images_attributes]={} if params[:page][:images_attributes].nil?
+    super << [images_attributes: [:id, :caption, :image_page_id]]
+  end
 end
+
+Refinery::Admin::PagesController.prepend(RefineryPageImagesAddRefineryAdminPagesControllerParams) rescue NameError
